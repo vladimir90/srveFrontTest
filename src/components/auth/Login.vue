6d40a9f7200267bb3100b47165a2c3a316fca07a
vladimir90 <vladimir.greksa@gmail.com>
2017-05-31 13:09:41 +0200

    sanitize

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -8,20 +8,24 @@
           <div class="field">
             <label class="label">Email:</label>
             <p class="control">
-              <input class="input" type="text" placeholder="Enter email">
+              <input class="input" type="text" placeholder="Enter email" v-model="userData.email">
             </p>
+            <p class="help is-danger">{{errorMsg.email}}</p>
+            <p class="help is-success">{{successMsg.email}}</p>
           </div>
 
           <div class="field">
             <label class="label">Password:</label>
             <p class="control">
-              <input class="input" type="password" placeholder="Enter password">
+              <input class="input" type="password" placeholder="Enter password" v-model="userData.password">
             </p>
+            <p class="help is-danger">{{errorMsg.password}}</p>
+            <p class="help is-success">{{successMsg.password}}</p>
           </div>
 
           <div class="field is-grouped">
             <p class="control">
-              <button class="button is-primary">Login</button>
+              <button class="button is-primary" @click.prevent="loginSubmit">Login</button>
             </p>
             <p class="control">
               <a href="/register" class="button">Register</a>
@@ -36,9 +40,81 @@
 </template>
 
 <script>
-  export default {
 
-  }
+export default {
+    data(){
+      return{
+            success:false,
+            userData:{
+                email:'',
+                password:''
+            },
+            errorMsg:{
+                email:'',
+                password:''
+            },
+            successMsg:{
+                email:'',
+                password:''
+            }
+        }
+    },
+
+    methods:{
+        sanitization(){
+
+            for (const key of Object.keys(this.errorMsg)) {
+              let value = this.errorMsg[key];
+                if(value.length > 0){
+                    value = '';
+                    this.errorMsg[key] = value;
+                }
+            }
+
+            if(this.userData.email === ''){
+                this.errorMsg.email = "Provide your email please";
+                return false;
+            }
+
+            if(this.userData.email.length < 6){
+                this.errorMsg.email = "Your email must have at least 6 chars!";
+                return false;
+            }
+
+            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.userData.email)){
+                  this.successMsg.email = "Email is correct";
+            } else{
+                  this.errorMsg.email = 'You have entered wrong email address';
+                  return false;
+            }
+
+            if(this.userData.password === ''){
+              this.errorMsg.password = "Your password field is empty";
+              return false;
+            }
+
+            if(this.userData.password.length < 6){
+                this.errorMsg.password = "Your password must have at least 6 chars";
+                return false;
+            }
+
+            this.successMsg.password = "Password correct";
+            return true;
+
+        },
+        loginSubmit(){
+            if(this.sanitization()){
+              //validation
+              //ajaxCall
+              //savingToken -> redirectToHome
+            }
+        }
+    }
+}
+
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -9,22 +9,37 @@
             <div class="field">
               <label class="label">Firstname:</label>
               <p class="control">
-                <input class="input" type="text" placeholder="Your firstname">
+                <input class="input" type="text" placeholder="Your firstname" v-model="userData.firstname">
               </p>
+              <p class="help is-danger">{{errorMsg.firstname}}</p>
+              <p class="help is-success">{{successMsg.firstname}}</p>
             </div>
 
             <div class="field">
               <label class="label">Lastname:</label>
               <p class="control">
-                <input class="input" type="text" placeholder="Your lastname">
+                <input class="input" type="text" placeholder="Your lastname" v-model="userData.lastname">
               </p>
+              <p class="help is-danger">{{errorMsg.lastname}}</p>
+              <p class="help is-success">{{successMsg.lastname}}</p>
+            </div>
+
+            <div class="field">
+              <label class="label">Email:</label>
+              <p class="control">
+                <input class="input" type="email" placeholder="Your email" v-model="userData.email">
+              </p>
+              <p class="help is-danger">{{errorMsg.email}}</p>
+              <p class="help is-success">{{successMsg.success}}</p>
             </div>
 
             <div class="field">
               <label class="label">Password:</label>
               <p class="control">
-                <input class="input" type="password" placeholder="Enter password">
+                <input class="input" type="password" placeholder="Enter password" v-model="userData.password">
               </p>
+              <p class="help is-danger">{{errorMsg.password}}</p>
+              <p class="help is-success">{{successMsg.password}}</p>
             </div>
 
             <div class="field">
@@ -40,7 +55,7 @@
             <div class="field is-grouped">
 
               <p class="control">
-                <a href="/home" class="button is-primary">Register</a>
+                <a href="/home" class="button is-primary" @click.prevent="registerSubmit">Register</a>
               </p>
             </div>
 
@@ -53,6 +68,97 @@
 
   <script>
     export default {
+      data(){
+        return{
+          success:false,
+          userData:{
+            firstname:'',
+            lastname:'',
+            email:'',
+            password:''
+          },
+          errorMsg:{
+            firstname:'',
+            lastname:'',
+            email:'',
+            password:''
+          },
+          successMsg:{
+            firstname:'',
+            lastname:'',
+            email:'',
+            password:''
+          }
+        }
+      },
+
+      methods:{
+        sanitization(){
+
+          for (const key of Object.keys(this.errorMsg)) {
+            let value = this.errorMsg[key];
+            if(value.length > 0){
+              value = '';
+              this.errorMsg[key] = value;
+            }
+          }
+
+          if(this.userData.firstname === ''){
+              this.errorMsg.firstname = 'Enter your firstname';
+              return false;
+          }
+          if(this.userData.firstname.length < 3){
+              this.errorMsg.firstname = 'Your fistname is to short';
+              return false;
+          }
+
+          if(this.userData.lastname === ''){
+            this.errorMsg.lastname = 'Enter your lastname';
+            return false;
+          }
+          if(this.userData.lastname.length < 3){
+            this.errorMsg.lastname = 'Your lastname is to short';
+          }
+
+          if(this.userData.email === ''){
+            this.errorMsg.email = "Provide your email please";
+            return false;
+          }
+
+          if(this.userData.email.length < 6){
+            this.errorMsg.email = "Your email must have at least 6 chars!";
+            return false;
+          }
+
+          if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.userData.email)){
+            this.successMsg.email = "Email is correct";
+          } else{
+            this.errorMsg.email = 'You have entered wrong email address';
+            return false;
+          }
+
+          if(this.userData.password === ''){
+            this.errorMsg.password = "Your password field is empty";
+            return false;
+          }
+
+          if(this.userData.password.length < 6){
+            this.errorMsg.password = "Your password must have at least 6 chars";
+            return false;
+          }
+
+
+          return true;
+
+        },
+        registerSubmit(){
+            this.sanitization();
+            //validation
+            //ajaxCall
+            //savingToken -> redirectToHome
+
+        }
+      }
 
     }
   </script>
